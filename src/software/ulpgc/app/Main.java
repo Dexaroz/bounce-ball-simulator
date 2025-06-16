d73fcfd61339d147a9ce1b69d800c24ce3e4bdb3
edumarrero <edumarreroglezz@gmail.com>
2025-06-16 17:18:07 +0200

    Changing display!

--- src/software/ulpgc/app/Main.java
+++ src/software/ulpgc/app/Main.java
@@ -10,9 +10,9 @@ public class Main {
     public static void main(String[] args) {
         MainFrame mainFrame = new MainFrame();
         BallPresenter presenter = new BallPresenter( mainFrame.getBallDisplay(), new BallSimulator( 0.001))
-                .add(new Ball("1",-10, 0.8, 25, 0, -9.8, 0.8, Color.RED))
-                .add(new Ball("2",7,0.8, 25, 0, -1.625, 0.5, Color.BLUE))
-                .add(new Ball("3",2,0.8, 10, 0, -24.79, 0.7, Color.GREEN));
+                .add(new Ball("1",0, 0.8, 25, 0, -9.8, 0.8, Color.RED))
+                .add(new Ball("2",-4,0.8, 25, 0, -1.625, 0.5, Color.BLUE))
+                .add(new Ball("3",4,0.8, 10, 0, -24.79, 0.7, Color.GREEN));
         presenter.execute();
         mainFrame.setVisible(true);
     }
